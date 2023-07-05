57874fa5f2268af90cb6d162b28dc47ae941d68a
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-07-05 19:11:32 -0400

    add

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+import React from "react";
+ 
+//want to create a button that will listen to 2 events and console log 
+// so i have 1 function 
+// the constants are functions that will trigger when they are called, depending on the event 
+function EyesOnMe() {
+
+    const Focus = () => {      //these are const, that i have to invoke the functions of
+        console.log("good")
+    }
+
+    const Blur = () => {
+        console.log("Hey! Eyes on Me!")
+    }
+
+    // the return value for the overall function -> will always return a JSX. here the addlistener event is writen and is listing for it. 
+    return (
+        <div>
+            <button onFocus={Focus} onBlur={Blur}>Eyes on me</button> 
+         
+       </div>
+    )
+}
+
+
+    
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
 // Code Keypad Component Here
+import React from "react";    
+//getting react from react. basic set 
 
+//here we want to create an event listener function. 
+//create a function called handleChange, that will do the think we want after the event is triggered 
+// in the return we have the event listener 
+// we always need to return a JSX value 
 function Keypad (){
+    const handleChange = () => {
+        console.log("Entering password...")
+    }
+    
     return (
-        <div></div>
-    )
+        <div>
+             <input type="password" onChange= {handleChange} />
+        </div>   
+    );
 }
 
 export default Keypad;
+
+
+
